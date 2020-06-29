a0ea46673fa44cbb70a34872c104ce9b3e577820
flaviomoura <contato@flaviomoura.mat.br>
2020-06-29 14:56:23 -0300

    new init.el

--- init.el
+++ init.el
@@ -1,2 +1,19 @@
-(org-babel-load-file "config.org")
+;; add MELPA package server
+(require 'package)
+
+(add-to-list 'package-archives 
+  '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(package-initialize)
+
+;; if not yet installed, install package use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+;; load org package and our emacs-config.org file
+(require 'org)
+(org-babel-load-file "config.org") 
 
