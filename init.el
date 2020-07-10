5ac46129b41442f982c73536b944ecdf7842f52f
flaviomoura <contato@flaviomoura.mat.br>
2020-07-10 17:00:36 -0300

    cleanup

--- init.el
+++ init.el
@@ -17,19 +17,3 @@
 (require 'org)
 (org-babel-load-file "config.org") 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method 'synctex)
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   '(vscode-dark-plus-theme use-package smartparens proof-general org-roam-bibtex org-ref org-plus-contrib org-noter-pdftools org-journal org-download magit git-timemachine git-gutter flycheck-irony company-irony company-coq bbdb auctex)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
