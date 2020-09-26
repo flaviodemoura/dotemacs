50d75ad83b1bcc3dbd8f922721f97fe4a86db6ca
flaviomoura <contato@flaviomoura.mat.br>
2020-09-26 16:13:16 -0300

    org-roam

--- init.el
+++ init.el
@@ -1,19 +1,29 @@
-;; add MELPA package server
 (require 'package)
-
-(add-to-list 'package-archives 
-  '("melpa" . "http://melpa.milkbox.net/packages/"))
-
-(unless package-archive-contents
-  (package-refresh-contents))
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 
-;; if not yet installed, install package use-package
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 
-;; load org package and our emacs-config.org file
-(require 'org)
-(org-babel-load-file "config.org") 
+(org-babel-load-file (expand-file-name "~/emacs1/.emacs.d/config.org"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(package-selected-packages
+   '(company-org-roam org-roam-bibtex org-roam orgit zenburn-theme vscode-dark-plus-theme use-package ubuntu-theme try smartparens proof-general ox-reveal org-ref org-noter org-journal modus-vivendi-theme modus-operandi-theme magit git-timemachine git-gutter flycheck-irony company-irony company-coq bbdb auctex ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
