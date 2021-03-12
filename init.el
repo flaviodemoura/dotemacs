184a711cda28e722a3d1f59d497e4735ae5aab27
flaviomoura <contato@flaviomoura.mat.br>
2021-03-12 12:58:38 -0300

    cleaning for faster startup

--- init.el
+++ init.el
@@ -11,19 +11,3 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method 'synctex)
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   '(ox-reveal org-journal org-noter org-roam-bibtex org-roam org-ref orgit use-package try smartparens proof-general pdf-tools modus-vivendi-theme magit git-timemachine git-gutter flycheck-irony company-irony bbdb auctex ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
