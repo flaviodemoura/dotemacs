fc62b7108c5b30409f3ccdf9b0295a5c3945e92e
flaviomoura <contato@flaviomoura.mat.br>
2021-07-16 04:57:01 -0300

    dropbox config

--- init.el
+++ init.el
@@ -11,19 +11,3 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method 'synctex)
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   '(flycheck-grammarly flymake-grammarly grammarly org-gcal use-package try smartparens proof-general ox-reveal orgit org-roam-bibtex org-noter org-journal modus-vivendi-theme git-timemachine git-gutter flycheck-irony company-irony bbdb auctex ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
