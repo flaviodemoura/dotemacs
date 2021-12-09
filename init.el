b9f222f934f8292c035baec3064ae2c6fe6d2c2b
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2021-12-08 21:59:08 -0300

    pdf-tools commented

--- init.el
+++ init.el
@@ -12,20 +12,3 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (require 'org)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method (quote synctex))
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   (quote
-    (org-roam-bibtex org-roam use-package proof-general pdf-tools orgit org-journal git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
