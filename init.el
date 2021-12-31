a639048108fb26018c8efe563eeeda5086b7aa63
Flávio Leonardo Cavalcanti de Moura <flaviomoura@pop-os.localdomain>
2021-12-31 14:27:27 -0300

    ajustes

--- init.el
+++ init.el
@@ -9,26 +9,9 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(require 'quelpa-use-package)
+;; (require 'quelpa-use-package)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (require 'org)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method (quote synctex))
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   (quote
-    (org-xournalpp org-krita quelpa-use-package proof-general pdf-tools orgit org-roam org-journal git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
