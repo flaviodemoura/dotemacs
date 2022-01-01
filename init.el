d089b5235e41d4b1194664d18fefe71c1a32b8f1
Flávio Leonardo Cavalcanti de Moura <flaviomoura@pop-os.localdomain>
2022-01-01 06:34:08 -0300

    org-ref v3 config

--- init.el
+++ init.el
@@ -15,3 +15,19 @@
 
 (require 'org)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(package-selected-packages
+   '(ivy-bibtex counsel use-package proof-general orgit org-roam org-ref git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
