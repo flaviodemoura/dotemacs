1ed324aa9f7a6ce499699f4ce50f1603beb1886a
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-01-08 22:54:38 -0300

    configurations motivated by system crafters

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages")))
 
 (package-initialize)
 
@@ -9,7 +10,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; (require 'quelpa-use-package)
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
@@ -23,8 +28,41 @@
  '(TeX-source-correlate-method 'synctex)
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(package-selected-packages
-   '(ivy-bibtex counsel use-package proof-general orgit org-roam org-ref git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window)))
+   '(doom-themes doom-modeline zenburn-theme vscode-dark-plus-theme modus-operandi-theme modus-vivendi-theme org-xournalpp quelpa-use-package quelpa tuareg org-tree-slide simple-httpd ivy-bibtex counsel use-package proof-general orgit org-roam org-ref git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(proof-electric-terminator-enable t)
+ '(proof-three-window-mode-policy 'hybrid)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
