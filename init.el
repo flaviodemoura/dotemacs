0e0c8c69adeae13381815c86004bc67746b9ba8c
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-01-10 14:56:30 -0300

    better config

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
@@ -18,20 +19,3 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (require 'org)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method 'synctex)
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   '(zenburn-theme vscode-dark-plus-theme tuareg simple-httpd quelpa-use-package proof-general orgit org-xournalpp org-tree-slide org-roam org-ref ivy-bibtex git-timemachine git-gutter forge flycheck-irony doom-themes doom-modeline counsel company-irony bbdb auctex ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
