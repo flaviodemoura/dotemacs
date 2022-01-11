5a4c37b7e09c3866fa9bfac6a2d388e8ad2b0100
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-01-11 15:48:46 -0300

    maximized

--- init.el
+++ init.el
@@ -19,3 +19,19 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (require 'org)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(package-selected-packages
+   '(zenburn-theme vscode-dark-plus-theme tuareg simple-httpd quelpa-use-package proof-general orgit org-xournalpp org-tree-slide org-ref ivy-bibtex git-timemachine git-gutter forge flycheck-irony doom-themes doom-modeline counsel company-irony bbdb auctex ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
