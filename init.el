58e03a738a64248400e66169b18885e4309bb067
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-02-07 06:27:29 -0300

    ivy enable, counsel still disabled

--- init.el
+++ init.el
@@ -23,19 +23,3 @@
 
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-method 'synctex)
- '(TeX-source-correlate-mode t)
- '(TeX-source-correlate-start-server t)
- '(package-selected-packages
-   '(org-xournalpp org-tree-slide org-ref orgit zenburn-theme vscode-dark-plus-theme tuareg simple-httpd quelpa-use-package proof-general ivy-bibtex hydra htmlize git-timemachine git-gutter forge flycheck-irony doom-themes doom-modeline counsel company-irony citeproc bbdb auctex ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
