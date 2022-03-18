4c30aad5a7f065cfbc48360a63b8f22e46984da6
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-03-18 06:11:08 -0300

    update

--- init.el
+++ init.el
@@ -11,15 +11,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(quelpa
- '(quelpa-use-package
-   :fetcher git
-   :url "https://github.com/quelpa/quelpa-use-package.git"))
-(require 'quelpa-use-package)
+;; (quelpa
+;;  '(quelpa-use-package
+;;    :fetcher git
+;;    :url "https://github.com/quelpa/quelpa-use-package.git"))
+;; (require 'quelpa-use-package)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(require 'org)
 
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
 
