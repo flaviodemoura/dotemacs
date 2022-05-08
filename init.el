727b9c630852f36a0018d9317237f389ebf07e0e
Flávio Leonardo Cavalcanti de Moura <flaviomoura@pop-os.localdomain>
2022-05-08 05:02:46 -0300

    quelpa commented lines

--- init.el
+++ init.el
@@ -11,13 +11,29 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(quelpa
- '(quelpa-use-package
-   :fetcher git
-   :url "https://github.com/quelpa/quelpa-use-package.git"))
-(require 'quelpa-use-package)
+;; (quelpa
+;;  '(quelpa-use-package
+;;    :fetcher git
+;;    :url "https://github.com/quelpa/quelpa-use-package.git"))
+;; (require 'quelpa-use-package)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-source-correlate-method 'synctex)
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(package-selected-packages
+   '(htmlize use-package tuareg simple-httpd proof-general pdf-tools orgit org-tree-slide org-ref ivy-bibtex helm-bibtex git-timemachine git-gutter forge flycheck-irony company-irony bbdb auctex ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
