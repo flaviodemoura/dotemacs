f8a12f6a44090a9d14cfbeff88b3922e77817cfb
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2023-11-13 09:01:23 -0300

    global visual line mode

--- init.el
+++ init.el
@@ -61,16 +61,28 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
+(global-hl-line-mode 1)
+(global-visual-line-mode 1)
 
 (setq ring-bell-function 'ignore)
 
 (show-paren-mode 1)
 
-(use-package company :straight t)
-
-(use-package ef-themes
+(use-package proof-general
   :straight t
-  :config (load-theme 'ef-light t))  
+  :config
+  (eval-after-load "proof-script" '(progn
+                                     (define-key proof-mode-map [(C-down)] 
+						 'proof-assert-next-command-interactive)
+                                     (define-key proof-mode-map [(C-up)] 
+						 'proof-undo-last-successful-command))))
+(setq pg-hide-all-proofs t)
+(setq proof-electric-terminator-enable t)
+(setq proof-three-window-mode-policy 'hybrid)
+
+
+(use-package company :straight t)
+(use-package company-coq :straight t)
 
 (use-package ivy
     :straight t
@@ -113,142 +125,131 @@
 (use-package forge
   :straight t)
 
-(use-package proof-general
-    :straight t
-    :config
-    (eval-after-load "proof-script" '(progn
-                                       (define-key proof-mode-map [(C-down)] 
-                                         'proof-assert-next-command-interactive)
-                                       (define-key proof-mode-map [(C-up)] 
-                                         'proof-undo-last-successful-command))))
-  (setq pg-hide-all-proofs t)
-  (setq proof-electric-terminator-enable t)
-  (setq proof-three-window-mode-policy 'hybrid)
 
-    (defun zp/org-find-time-file-property (property &optional anywhere)
-      "Return the position of the time file PROPERTY if it exists.
+(defun zp/org-find-time-file-property (property &optional anywhere)
+  "Return the position of the time file PROPERTY if it exists.
         When ANYWHERE is non-nil, search beyond the preamble."
-      (save-excursion
-        (goto-char (point-min))
-        (let ((first-heading
-               (save-excursion
-                 (re-search-forward org-outline-regexp-bol nil t))))
-          (when (re-search-forward (format "^#\\+%s:" property)
-                                   (if anywhere nil first-heading)
-                                   t)
-            (point)))))
+  (save-excursion
+    (goto-char (point-min))
+    (let ((first-heading
+           (save-excursion
+             (re-search-forward org-outline-regexp-bol nil t))))
+      (when (re-search-forward (format "^#\\+%s:" property)
+                               (if anywhere nil first-heading)
+                               t)
+        (point)))))
 
-    (defun zp/org-has-time-file-property-p (property &optional anywhere)
-      "Return the position of time file PROPERTY if it is defined.
+(defun zp/org-has-time-file-property-p (property &optional anywhere)
+  "Return the position of time file PROPERTY if it is defined.
         As a special case, return -1 if the time file PROPERTY exists but
         is not defined."
-      (when-let ((pos (zp/org-find-time-file-property property anywhere)))
-        (save-excursion
-          (goto-char pos)
-          (if (and (looking-at-p " ")
-                   (progn (forward-char)
-                          (org-at-timestamp-p 'lax)))
-              pos
-            -1))))
+  (when-let ((pos (zp/org-find-time-file-property property anywhere)))
+    (save-excursion
+      (goto-char pos)
+      (if (and (looking-at-p " ")
+               (progn (forward-char)
+                      (org-at-timestamp-p 'lax)))
+          pos
+        -1))))
 
-    (defun zp/org-set-time-file-property (property &optional anywhere pos)
-      "Set the time file PROPERTY in the preamble.
+(defun zp/org-set-time-file-property (property &optional anywhere pos)
+  "Set the time file PROPERTY in the preamble.
         When ANYWHERE is non-nil, search beyond the preamble.
         If the position of the file PROPERTY has already been computed,
         it can be passed in POS."
-      (when-let ((pos (or pos
-                          (zp/org-find-time-file-property property))))
-        (save-excursion
-          (goto-char pos)
-          (if (looking-at-p " ")
-              (forward-char)
-            (insert " "))
-          (delete-region (point) (line-end-position))
-          (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
-            (insert now)))))
+  (when-let ((pos (or pos
+                      (zp/org-find-time-file-property property))))
+    (save-excursion
+      (goto-char pos)
+      (if (looking-at-p " ")
+          (forward-char)
+        (insert " "))
+      (delete-region (point) (line-end-position))
+      (let* ((now (format-time-string "[%Y-%m-%d %a %H:%M]")))
+        (insert now)))))
 
-    (defun zp/org-set-last-modified ()
-      "Update the LAST_MODIFIED file property in the preamble."
-      (when (derived-mode-p 'org-mode)
-        (zp/org-set-time-file-property "LAST_MODIFIED")))
+(defun zp/org-set-last-modified ()
+  "Update the LAST_MODIFIED file property in the preamble."
+  (when (derived-mode-p 'org-mode)
+    (zp/org-set-time-file-property "LAST_MODIFIED")))
 
-    (add-hook 'before-save-hook #'zp/org-set-last-modified)
+(add-hook 'before-save-hook #'zp/org-set-last-modified)
 
-  (setq org-file-apps '((auto-mode . emacs)
-                        ("\\.mm\\'" . default)
-                        ("\\.x?html?\\'" . system)
-                        ("\\.dvi\\'" . system)
-                        ("\\.pdf\\'" . "/usr/bin/okular %s")))
-  (setq org-startup-indented 'f)
-  (setq org-startup-folded t)
-  (setq org-return-follows-link  t)
-  (setq org-directory "~/workspace/org/")
-  (setq org-special-ctrl-a/e 't)
-  (setq org-default-notes-file (concat org-directory "notes.org"))
-  (setq org-src-fontify-natively 't)
-  (setq org-src-tab-acts-natively t)
-  (setq org-src-window-setup 'current-window)
-  (setq org-deadline-warning-days 90)
-  (setq org-agenda-files (directory-files-recursively "~/workspace/org" "\\.org$"))
-  (setq org-agenda-show-all-dates t)
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-agenda-start-with-clockreport-mode t)
-  (setq org-agenda-arquives-mode t)
-  (setq org-agenda-dim-blocked-tasks nil)
-  (setq org-agenda-inhibit-startup t)
-  (setq org-agenda-use-tag-inheritance nil)
-  (setq org-agenda-ignore-properties '(effort appt stats category))
-  (setq org-todo-keywords
-        '((type "TODO(t)" "PROGRESS(s@/!)" "WAITING(w@/!)" "READING(r)" "NEXT(n)" "|" "CANCELLED(c)" "DONE(d)" "READ(e)")))
+(setq org-file-apps '((auto-mode . emacs)
+                      ("\\.mm\\'" . default)
+                      ("\\.x?html?\\'" . system)
+                      ("\\.dvi\\'" . system)
+                      ("\\.pdf\\'" . "/usr/bin/okular %s")))
+(setq org-startup-indented 'f)
+(setq org-startup-folded t)
+(setq org-return-follows-link  t)
+(setq org-directory "~/workspace/org/")
+(setq org-special-ctrl-a/e 't)
+(setq org-default-notes-file (concat org-directory "notes.org"))
+(setq org-src-fontify-natively 't)
+(setq org-src-tab-acts-natively t)
+(setq org-src-window-setup 'current-window)
+(setq org-deadline-warning-days 90)
+(setq org-agenda-files (directory-files-recursively "~/workspace/org" "\\.org$"))
+(setq org-agenda-show-all-dates t)
+(setq org-agenda-start-with-log-mode t)
+(setq org-agenda-start-with-clockreport-mode t)
+(setq org-agenda-arquives-mode t)
+(setq org-agenda-dim-blocked-tasks nil)
+(setq org-agenda-inhibit-startup t)
+(setq org-agenda-use-tag-inheritance nil)
+(setq org-agenda-ignore-properties '(effort appt stats category))
+(setq org-todo-keywords
+      '((type "TODO(t)" "PROGRESS(s@/!)" "WAITING(w@/!)" "READING(r)" "NEXT(n)" "|" "CANCELLED(c)" "DONE(d)" "READ(e)")))
 
-  (global-set-key (kbd "C-c a") 'org-agenda)
-  (global-set-key (kbd "C-c b") 'org-iswitchb)
-  (global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+(global-set-key (kbd "C-c l") 'org-store-link)
 
-  (require 'ox-publish)
-  (setq org-html-validation-link nil
-        org-html-head-include-scripts nil
-        org-html-head-include-default-style nil
-        org-html-head "<link rel=\"stylesheet\" href=\"files/mystyle3.css\" />")
-  (setq org-publish-project-alist
-        '(("lc1"
-           :base-directory "~/workspace/LC1-github"
-           :base-extension "org"
-           :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
-           :publishing-function org-html-publish-to-html
-           :headline-levels 3
-           :section-numbers nil
-           :with-toc nil
-           :html-head "<link rel="stylesheet" type="text/css" href="files/site.css"/>"
-           :html-preamble t)
-
-            ("paa"
-             :base-directory "~/workspace/PAA-github"
-             :base-extension "org"
-             :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
-             :publishing-function org-html-publish-to-html
-             :headline-levels 3
-             :section-numbers nil
-             :with-toc nil
-             :html-head "<link rel="stylesheet" type="text/css" href="files/site.css"/>"
-             :html-preamble t)
-
-            ("webpage"
-             :recursive t
-             :base-directory "~/workspace/flaviodemoura.github.io/content/"
-             :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
-             :publishing-function 'org-html-publish-to-html
-             :with-author nil
-             :with-creator nil
-             :with-toc nil
-             :section-numbers nil
-             :time-stamp-file nil)
-
-            ("images"
-             :base-directory "~/workspace/org/jpeg/"
-             :base-extension "jpg\\|gif\\|png"
-             :publishing-directory "~/workspace/flaviodemoura.github.io/public/files"
-             :publishing-function org-publish-attachment)
-
-            ("ensino" :components ("webpage" "lc1" "paa" ))))
+(require 'ox-publish)
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"files/mystyle3.css\" />")
+(setq org-publish-project-alist
+      '(("lc1"
+         :base-directory "~/workspace/LC1-github"
+         :base-extension "org"
+         :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
+         :publishing-function org-html-publish-to-html
+         :headline-levels 3
+         :section-numbers nil
+         :with-toc nil
+         :html-head "<link rel="stylesheet" type="text/css" href="files/site.css"/>"
+         :html-preamble t)
+	
+        ("paa"
+         :base-directory "~/workspace/PAA-github"
+         :base-extension "org"
+         :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
+         :publishing-function org-html-publish-to-html
+         :headline-levels 3
+         :section-numbers nil
+         :with-toc nil
+         :html-head "<link rel="stylesheet" type="text/css" href="files/site.css"/>"
+         :html-preamble t)
+	
+        ("webpage"
+         :recursive t
+         :base-directory "~/workspace/flaviodemoura.github.io/content/"
+         :publishing-directory "~/workspace/flaviodemoura.github.io/public/"
+         :publishing-function 'org-html-publish-to-html
+         :with-author nil
+         :with-creator nil
+         :with-toc nil
+         :section-numbers nil
+         :time-stamp-file nil)
+	
+        ("images"
+         :base-directory "~/workspace/org/jpeg/"
+         :base-extension "jpg\\|gif\\|png"
+         :publishing-directory "~/workspace/flaviodemoura.github.io/public/files"
+         :publishing-function org-publish-attachment)
+	
+        ("ensino" :components ("webpage" "lc1" "paa" ))))
 
